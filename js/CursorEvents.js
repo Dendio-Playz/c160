4f4bb0d5aacaebe5d728e14a1a57501a521053d0
Dendio-Playz <93475249+Dendio-Playz@users.noreply.github.com>
2024-03-12 17:57:49 +0000

    Update CursorEvents.js

--- js/CursorEvents.js
+++ js/CursorEvents.js
@@ -11,16 +11,20 @@ AFRAME.registerComponent("cursor-listener", {
     //  Click Events
     this.el.addEventListener("click", evt => {
       const placesContainer = document.querySelector("#places-container");
+
       const { state } = placesContainer.getAttribute("tour");
 
       if (state === "places-list") {
+
         const id = this.el.getAttribute("id");
+
         const placesId = [
           "taj-mahal",
           "budapest",
           "new-york-city",
           "eiffel-tower"
         ];
+
         if (placesId.includes(id)) {
           placesContainer.setAttribute("tour", {
             state: "view",
@@ -28,35 +32,45 @@ AFRAME.registerComponent("cursor-listener", {
           });
         }
       }
-      if (state === "view"){
-        this.handleViewState()
+
+      if (state === "view") {
+        this.handleViewState();
       }
-      if (state === "change-view"){
-        this.handleViewState()
+      if (state === "change-view") {
+        this.handleViewState();
       }
     });
   },
+  handleViewState: function() {
+    const el = this.el;
 
-  handleViewState: function(){
-    const el = this.el
-    const id = el.getAttribute("id")
-    const placesContainer = document.querySelector("places-container")
-    const {selectedItemId} = placesContainer.getAttribute("cursor-listener")
-    const sideViewPlacesId = ["place-1","place-2","place-3","place-4"]
-    if (sideViewPlacesId.includes(id)){
-      placesContainer.setAttribute("tour",{
-        state:"change-view",      
-      })
-      const skyel = document.querySelector("#main-container")
-      skyel.setAttribute("material",{
-        src:`./assets/360_images/${selectedItemId}/${id}.jpg`,
-        color:"#FFF"
-      })
+    const id = el.getAttribute("id");
+
+    const placesContainer = document.querySelector("#places-container");
+
+    const { selectedItemId } = placesContainer.getAttribute("cursor-listener");
+
+    //Keeping all the images as id of the images with .jpg extension
+    const sideViewPlacesId = ["place-1", "place-2", "place-3", "place-4"];
+
+    if (sideViewPlacesId.includes(id)) {
+      
+      placesContainer.setAttribute("tour", {
+        state: "change-view"
+      });
+
+      const skyEl = document.querySelector("#main-container");
+      
+      //Set the 360 degree image to the sky element.
+      skyEl.setAttribute("material", {
+        src: `./assets/360_images/${selectedItemId}/${id}.jpg`,
+        color: "#fff"
+      });
+     
     }
   },
-
   handleMouseEnterEvents: function() {
-    // Mouse Center Events
+    // Mouse Enter Events
     this.el.addEventListener("mouseenter", () => {
       const placeContainer = document.querySelector("#places-container");
       const { state } = placeContainer.getAttribute("tour");
